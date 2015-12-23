e46c91738e91b553000a29aec23f0d92dc6ca82d
pascal <pascal@stic.ch>
2015-12-23 14:06:45 +0100

    removed :v1, changed license to free, added the cleanup part to remove the settings file

--- Casks/cloudapp.rb
+++ Casks/cloudapp.rb
@@ -1,11 +1,13 @@
-cask :v1 => 'cloudapp' do
+cask 'cloudapp' do
   version '3.7.0'
   sha256 '29b6ad9938c76e9007a66f156922777fd5ec3deb6ac6d3b3a6d147142d07cb9a'
 
   url "https://s3.amazonaws.com/downloads.getcloudapp.com/mac/CloudApp-#{version}.dmg"
   name 'CloudApp'
   homepage 'http://www.getcloudapp.com/'
-  license :commercial
+  license :free
 
   app 'CloudApp.app'
+
+  zap :delete => '~/Library/Preferences/com.linebreak.CloudAppMacOSX.plist'
 end
